d6454ea8fe0dc4ac64ca83f906b107584178add6
S <satria.pandega@gmail.com>
2025-04-15 15:21:54 +0700

    added ORDER BY

--- 1.sql
+++ 1.sql
@@ -5,3 +5,43 @@ SELECT name, UserId FROM Categories;
 -- SELECT DISTINCT statement is used to return only distinct (different) values.
 SELECT DISTINCT UserId FROM Categories;
 SELECT DISTINCT websiteName, city FROM Iplogs;
+
+-- The WHERE clause is used to filter records.
+SELECT * FROM Categories c  WHERE id = '1';
+
+-- ORDER BY ASC alphabetically if string or numerically
+SELECT * FROM Categories c  ORDER BY id ASC;
+
+-- ORDER BY DESC alphabetically if string or numerically
+SELECT * FROM Categories c  ORDER BY id DESC;
+
+-- SORTING IP
+SELECT 
+    ipAddress AS ip, 
+    INET6_ATON(ipAddress) AS ipBin,
+    'ipv4' AS ipType
+FROM IPLogs 
+UNION ALL
+SELECT 
+    ipAddress AS ip, 
+    INET6_ATON(ipAddress) AS ipBin,
+    'ipv6' AS ipType
+FROM IPLogs
+ORDER BY ipBin ASC;
+
+-- SORTING IP With Filter
+SELECT 
+    ipAddress AS ip, 
+    INET6_ATON(ipAddress) AS ipBin,
+    'ipv4' AS ipType
+FROM IPLogs 
+WHERE ipAddress LIKE '%.%.%.%'
+  AND LENGTH(ipAddress) - LENGTH(REPLACE(ipAddress, '.', '')) = 3
+UNION ALL
+SELECT 
+    ipAddress AS ip, 
+    INET6_ATON(ipAddress) AS ipBin,
+    'ipv6' AS ipType
+FROM IPLogs
+WHERE LENGTH(ipAddress) - LENGTH(REPLACE(ipAddress, ':', '')) BETWEEN 2 AND 7
+ORDER BY ipBin ASC;
